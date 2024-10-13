a58cebb8a24dcfff575314a4f9c27014c8b41861
zhesnaile <carlosojar@gmail.com>
2024-10-14 00:48:09 +0200

    [DEL] unused block of variables

--- demo2_docker_vm/terraform/instances/docker-example-vm/vars.tf
+++ demo2_docker_vm/terraform/instances/docker-example-vm/vars.tf
@@ -1,22 +1,3 @@
-variable "s3_bucket_config" {
-  type = object({
-    bucket          = string # bucket name
-    key             = string # path in the bucket to store tfstate
-    region          = string # s3 region
-    endpoint        = string # s3 endpoint url
-    skip_aws_checks = bool   # skips a series of checks that are only relevant to AWS and break compatibility with other object storage solutions
-  })
-  description = "Configuration for the S3 bucket used to store Terraform state."
-
-  default = {
-    bucket          = ""
-    key             = ""
-    region          = "eu-central"
-    endpoint        = "https://s3.eu-central-003.backblazeb2.com"
-    skip_aws_checks = true
-  }
-}
-
 variable "cloud_init_template" {
   type        = string
   description = "Name of the cloud-init template to use"
